c95f71b731e7f7339afa0f0f7f598e3cb6c379f1
Equalibrum <tomaszbru@gmail.com>
2016-03-27 00:22:19 +0100

    first commit

---
+++ Source/Equalix/Equalix.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Equalix.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Equalix, "Equalix" );
---
+++ Source/Equalix/EqualixGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EqualixGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EQUALIX_API AEqualixGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
